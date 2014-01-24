89804b5e46e328e7565bd9c355c10d1bf749cb4f
Justas Azna <justas.azna@alexandra.dk>
2014-01-24 17:59:34 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+	name='ckanext-realtime',
+	version=version,
+	description="CKAN extension which enables CKAN to serve realtime data",
+	long_description=open('README.rst').read(),
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Justas Azna',
+	author_email='justas.azna@gmail.dk',
+	url='',
+	license='AGPL',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.realtime'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+    realtime=ckanext.realtime.plugin:RealtimePlugin
+	""",
+)
