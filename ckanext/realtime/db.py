b84a15a46bcdc126f9f60ed96ed85708dcb401d4
Justas Azna <justas.azna@alexandra.dk>
2014-02-05 16:17:10 +0100

    add ckanext.realtime.db module

---
+++ ckanext/realtime/db.py
@@ -0,0 +1,78 @@
+import logging
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+from ckanext.realtime.plugin import RealtimeException
+
+log = logging.getLogger(__name__)
+
+_engines = {}
+    
+    
+def _get_engine(connection_url):
+    '''Get either read or write engine.'''
+    engine = _engines.get(connection_url)
+
+    if not engine:
+        engine = sqlalchemy.create_engine(connection_url)
+        _engines[connection_url] = engine
+    return engine
+
+class SessionFactory(object):
+    ''' Factory class which makes sqlalchemy orm sessions '''
+    _configured = False
+    
+    _configuration_error_msg = 'SessionFactory has not been configured'
+    
+    _ReadSession = sessionmaker()
+    _WriteSession = sessionmaker()
+    
+    @classmethod
+    def configure(cls, read_connection_url, write_connection_url):
+        ''' Configure SessionFactory
+        
+        **Parameters:**
+        :param read_connection_url: sqlalchemy url of connection used for 
+            reading
+        :type read_connection_url: string
+        :param read_connection_url: sqlalchemy url of connection used for 
+            writing
+        :type read_connection_url: string 
+        '''
+        cls._configured = True
+        
+        read_engine = _get_engine(read_connection_url)
+        cls._ReadSession.configure(bind=read_engine)
+            
+        write_engine = _get_engine(write_connection_url)
+        cls._WriteSession.configure(bind=write_engine)
+    
+    @classmethod
+    def get_read_session(cls):
+        ''' Makes an sql alchemy orm session for reading.
+        You have to configure the SessionFactory class before calling this
+        method.
+        
+        **Returns:**
+        :return: orm session associated with read engine
+        :rtype: sqlalchemy.orm.session.Session
+        '''
+        if not cls._configured:
+            raise RealtimeException(cls._configuration_error_msg)
+        return cls._ReadSession()
+        
+    
+    @classmethod
+    def get_write_session(cls):
+        ''' Makes an sql alchemy orm session for writing.
+        You have to configure the SessionFactory class before calling this
+        method.
+        
+        **Returns:**
+        :return: orm session associated with write engine
+        :rtype: sqlalchemy.orm.session.Session
+        '''
+        if not cls._configured:
+            raise RealtimeException(cls._configuration_error_msg)
+        return cls._WriteSession()
