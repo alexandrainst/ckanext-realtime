25ea2cd5f93fd0f0914e757d4b70b88d2e1e45af
Justas Azna <justas.azna@alexandra.dk>
2014-02-13 13:34:38 +0100

    change notify decorator to take in event_factory_class & **kwargs

--- ckanext/realtime/logic/__init__.py
+++ ckanext/realtime/logic/__init__.py
@@ -1,21 +1,23 @@
 import functools
 
-def notify(dispatcher_class, event_class):
+def notify(dispatcher_class, event_factory_class, **kwargs):
     '''A decorator that notifies listeners about executed ckan API actions.
     
     This decorator should be applied to ckan API action functions which accept
     2 parameters: context and data_dict. It notifies the listeners after(if)
     the full body of the decorated function has been executed.
     
-    :param dispatcher_class: a class with a classmethod *dispatch(event)*
-    :param event_class: a class for constructing events out of *data_dict*
+    :param dispatcher_class: a class with a classmethod *dispatch(events)*
+    :param event_factory_class: a factory class for constructing events out of *data_dict*
+        and any additional *kwargs*
+    :param **kwargs: aditional arguments for the specified event factory class
     
     '''
     def actual_decorator(action_func):
         @functools.wraps(action_func)
         def wrapper(context, data_dict):
             result = action_func(context, data_dict)
-            dispatcher_class.dispatch(event_class(data_dict))
+            dispatcher_class.dispatch(event_factory_class.build_events(data_dict, **kwargs))
             return result
         return wrapper
     return actual_decorator
