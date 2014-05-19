4f64874c7dbfa5286f88bfb59ee52272968b9109
Justas Azna <justas.azna@alexandra.dk>
2014-05-19 18:34:13 +0200

    datastores are made into observable datastore automatically on the first subscription

--- ckanext/realtime/logic/action.py
+++ ckanext/realtime/logic/action.py
@@ -60,7 +60,10 @@ def datastore_make_observable(context, data_dict):
     
     p.toolkit.check_access('datastore_make_observable', context, data_dict)
 
+    if not _datastore_exists(data_dict):
+        return {'success': False}
     db.add_datastore_notifier_trigger(data_dict['resource_id'])
+    return {'success': True}
     
     
 def realtime_check_observable_datastore(context, data_dict):
